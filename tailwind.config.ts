ba7225f99cc3659d39c2640d60de72d3f4a4ef8f
petermwash <petermwash48@gmail.com>
2024-02-27 16:30:10 +0300

    First Commit Ever

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        move: {
+          '0%, 49.99%': { zIndex: '1', opacity: '0' },
+          '50%, 100%': { zIndex: '5', opacity: '1' },
+        }
+      }
     },
   },
   plugins: [],
